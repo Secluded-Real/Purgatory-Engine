7d2ea243ce8332514437f3621e5bcf9b34d70b39
Secluded <thesecludedshow@gmail.com>
2025-08-06 00:15:57 -0400

    revert that shit

--- Purgatory Engine/FNF-PsychEngine-0.6.3/source/FreeplayState.hx
+++ Purgatory Engine/FNF-PsychEngine-0.6.3/source/FreeplayState.hx
@@ -138,13 +138,10 @@ class FreeplayState extends MusicBeatState
 		WeekData.setDirectoryFromWeek();
 
 		scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
-		scoreText.setFormat(Paths.font("comic.ttf"), 32, FlxColor.WHITE, RIGHT);
-		scoreText.x = 20;
+		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 
-		var scoreBG:FlxSprite = new FlxSprite(scoreText.x - 6, 0).makeGraphic(Std.int(FlxG.width * 1), 66, 0xFF000000);
-		scoreBG.alpha = 0.5;
-		scoreBG.screenCenter(X);
-		scoreBG.y = 10;
+		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
+		scoreBG.alpha = 0.6;
 		add(scoreBG);
 
 		diffText = new FlxText(scoreText.x, scoreText.y + 36, 0, "", 24);
